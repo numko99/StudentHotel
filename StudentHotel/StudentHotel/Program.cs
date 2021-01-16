efef915daee1678cc4b0726e976695d93e234518
numko99 <61965732+numko99@users.noreply.github.com>
2021-01-17 00:04:19 +0100

    nesto za jaska

--- StudentHotel/StudentHotel/Program.cs
+++ StudentHotel/StudentHotel/Program.cs
@@ -13,7 +13,7 @@ namespace StudentHotel
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();   
+            CreateHostBuilder(args).Build().Run();   //debiluuuuu
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
